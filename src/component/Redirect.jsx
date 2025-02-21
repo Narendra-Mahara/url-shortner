3dc7ee9954d11e434f64e84991d9379e6fcf8616
Narendra Mahara <narendramahara85131@gmail.com>
2025-02-21 17:33:38 +0545

    feat: add redirection

--- src/component/Redirect.jsx
+++ src/component/Redirect.jsx
@@ -1,13 +1,32 @@
-import React from "react";
-import { useParams } from "react-router";
-
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { databases } from "../lib/appwrite";
+import { Query } from "appwrite";
 const Redirect = () => {
+  const navigate = useNavigate();
   let { shortID } = useParams();
-  console.log(shortID);
+
+  useEffect(() => {
+    const getURL = async () => {
+      const response = await databases.listDocuments(
+        import.meta.env.VITE_APPWRITE_DATABASE_ID,
+        import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+        [Query.equal("shortURL", shortID)]
+      );
+
+      const originalURL = response.documents[0]?.originalURL || undefined;
+      if (originalURL == undefined) {
+        navigate("/");
+      } else {
+        window.location.replace(originalURL);
+      }
+    };
+    getURL();
+  }, [shortID]);
 
   return (
     <div>
-      
+      <h2 className="p-2">Redirecting...</h2>
     </div>
   );
 };
